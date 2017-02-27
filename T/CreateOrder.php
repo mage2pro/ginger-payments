873354ea6a2423dcafe4131270ebc58386ba9b1c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-27 12:20:21 +0300

    no message

---
+++ T/CreateOrder.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\GingerPayments\T;
+// 2017-02-27
+final class СreateOrder extends \Df\GingerPaymentsBase\T\CreateOrder {
+	/** 2017-02-27 @test */
+	function t01_ideal() {parent::t01_ideal();}
+}
