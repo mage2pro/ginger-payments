21e7baa8bb7a562cd5328dfa557092ed803a5443
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-27 12:58:08 +0300

    no message

---
+++ T/GetIdealIssuers.php
@@ -0,0 +1,5 @@
+<?php
+namespace Dfe\GingerPayments\T;
+// 2017-02-27
+// «[Ginger Payments] An example of a response to GET /v1/ideal/issuers/»: https://mage2.pro/t/3387
+final class GetIdealIssuers extends \Df\GingerPaymentsBase\T\GetIdealIssuers {}
