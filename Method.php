a472c88b9b8396272e0dcfe3ad48975d612de36d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-28 18:37:22 +0300

    no message

--- Method.php
+++ Method.php
@@ -1,4 +1,17 @@
 <?php
 // 2017-02-25
 namespace Dfe\GingerPayments;
-final class Method extends \Df\GingerPaymentsBase\Method {}
+final class Method extends \Df\GingerPaymentsBase\Method {
+	/**
+	 * 2017-02-28
+	 * Kassa Compleet and Ginger Payments use different formats
+	 * for the «order_lines/order_line/vat_percentage» property
+	 * of a «POST /v1/orders/» request: https://mage2.pro/t/3451
+	 * «"minimum": 0, "maximum": 100».
+	 * @override
+	 * @see \Df\GingerPaymentsBase\Method::vatFactor()
+	 * @used-by \Df\GingerPaymentsBase\T\CreateOrder::t01_success()
+	 * @return int
+	 */
+	function vatFactor() {return 1;}
+}
