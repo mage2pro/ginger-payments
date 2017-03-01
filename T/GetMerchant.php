cdf640f1c627f357f4ad92af1f2bfec00f0f7b09
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 10:57:21 +0300

    no message

--- T/GetMerchant.php
+++ T/GetMerchant.php
@@ -7,4 +7,21 @@ namespace Dfe\GingerPayments\T;
  * lead to the «You don't have the permission to access the requested resource» response?
  * https://mage2.pro/t/3457
  */
-final class GetMerchant extends \Df\GingerPaymentsBase\T\GetMerchant {}
+final class GetMerchant extends \Df\GingerPaymentsBase\T\GetMerchant {
+	/**
+	 * 2017-03-01
+	 * @override
+	 * @see \Df\GingerPaymentsBase\T\GetMerchant::merchantId()
+	 * @used-by \Df\GingerPaymentsBase\T\GetMerchant::t01()
+	 * @return string
+	 */
+	protected function merchantId() {return 'e563c46f';}
+
+	/**
+	 * 2017-03-01
+	 * @see \Df\GingerPaymentsBase\T\GetMerchant::projectId()
+	 * @used-by \Df\GingerPaymentsBase\T\GetMerchant::t01()
+	 * @return string
+	 */
+	protected function projectId() {return '6dc226ae-3924-4c61-bb02-49ad000fa961';}
+}
