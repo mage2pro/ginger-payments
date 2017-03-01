165d3d316858ec9de37a635e576bdd069205bd98
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 22:29:05 +0300

    no message

--- Source/Option.php
+++ Source/Option.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dfe\GingerPayments\Source;
+use GingerPayments\Payment\Order\Transaction\PaymentMethod as M;
 // 2017-03-01
+// [Ginger Payments] Available payment options: https://mage2.pro/t/3463
 /** @method static Option s() */
 final class Option extends \Df\GingerPaymentsBase\Source\Option {
 	/**
@@ -10,5 +12,12 @@ final class Option extends \Df\GingerPaymentsBase\Source\Option {
 	 * @used-by \Df\GingerPaymentsBase\Source\Option::map()
 	 * @return array(string => string)
 	 */
-	protected function mapExtra() {return [];}
+	protected function mapExtra() {return [
+		// 2017-03-01
+		// What is Bancontact? https://mage2.pro/t/3395
+		M::BANCONTACT => 'Bancontact'
+		// 2017-03-01
+		// What is SOFORT and how does it work? https://mage2.pro/t/3425
+		,M::SOFORT => 'SOFORT'
+	];}
 }
