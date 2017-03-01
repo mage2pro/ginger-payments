061faebbbdb13558d6bf9fcf58772f648a23971a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 21:08:59 +0300

    no message

--- Source/Option.php
+++ Source/Option.php
@@ -2,4 +2,13 @@
 namespace Dfe\GingerPayments\Source;
 // 2017-03-01
 /** @method static Option s() */
-class Option extends \Df\GingerPaymentsBase\Source\Option {}
+final class Option extends \Df\GingerPaymentsBase\Source\Option {
+	/**
+	 * 2017-03-01
+	 * @override
+	 * @see \Df\GingerPaymentsBase\Source\Option::mapExtra()
+	 * @used-by \Df\GingerPaymentsBase\Source\Option::map()
+	 * @return array(string => string)
+	 */
+	protected function mapExtra() {return [];}
+}
