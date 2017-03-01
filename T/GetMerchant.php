05838a3cd94b4be0f1a01f5c45dce2710172e84e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 10:42:28 +0300

    no message

---
+++ T/GetMerchant.php
@@ -0,0 +1,10 @@
+<?php
+namespace Dfe\GingerPayments\T;
+/**
+ * 2017-03-01
+ * https://www.gingerpayments.com/docs#_merchants
+ * [Ginger Payments] Why does a «GET merchants/self/projects/self/» request
+ * lead to the «You don't have the permission to access the requested resource» response?
+ * https://mage2.pro/t/3457
+ */
+final class GetMerchant extends \Df\GingerPaymentsBase\T\GetMerchant {}
