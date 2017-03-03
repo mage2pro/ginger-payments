64ec6d82b1dd139f7cfc7aeab250ddfd319f562b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 12:07:41 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,2 +1,3 @@
 // 2017-03-03
+// @see https://github.com/mage2pro/ginger-payments-base/blob/0.0.7/view/frontend/web/main.js?ts=4#L8
 define(['Df_GingerPaymentsBase/main'], function(parent) {return parent.extend({});});
