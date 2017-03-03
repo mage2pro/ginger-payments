0b257825cff8ad6d80663b76acc636eb571c819e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 16:44:38 +0300

    0.0.8

--- ConfigProvider.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Dfe\GingerPayments;
-// 2017-03-03
-// @used-by https://github.com/mage2pro/ginger-payments/blob/0.0.7/etc/frontend/di.xml?ts=4#L9
-final class ConfigProvider extends \Df\GingerPaymentsBase\ConfigProvider {}
