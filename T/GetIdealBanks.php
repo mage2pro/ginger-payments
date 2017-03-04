89db368a6582749cfe1581efc4d43e7f49a6fe2f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-04 09:36:20 +0300

    0.1.0

--- T/GetIdealBanks.php
+++ T/GetIdealBanks.php
@@ -2,4 +2,4 @@
 namespace Dfe\GingerPayments\T;
 // 2017-02-27
 // «[Ginger Payments] An example of a response to GET /v1/ideal/issuers/»: https://mage2.pro/t/3387
-final class GetIdealIssuers extends \Df\GingerPaymentsBase\T\GetIdealIssuers {}
+final class GetIdealBanks extends \Df\GingerPaymentsBase\T\GetIdealBanks {}
