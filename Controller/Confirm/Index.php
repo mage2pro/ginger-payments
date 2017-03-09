f4409fc11696017a28749f288ff1588c6d8326be
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 17:13:03 +0300

    no message

--- Controller/Confirm/Index.php
+++ Controller/Confirm/Index.php
@@ -1,5 +1,5 @@
 <?php
 namespace Dfe\GingerPayments\Controller\Confirm;
 // 2017-03-07
-/** @final Unable to use the PHP «final» keyword because of the M2 code generation. */
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Index extends \Df\GingerPaymentsBase\Controller\Confirm {}
