a81cc4bc347b8776441c0972954bfeba5a636ddd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-14 09:26:43 +0300

    0.4.0

---
+++ W/Handler.php
@@ -0,0 +1,4 @@
+<?php
+namespace Dfe\GingerPayments\W;
+// 2017-03-09
+final class Handler extends \Df\GingerPaymentsBase\W\Handler {}
--- Webhook.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Dfe\GingerPayments;
-// 2017-03-09
-final class Webhook extends \Df\GingerPaymentsBase\Webhook {}
