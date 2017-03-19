d27458650c4e34e273c7e4aaa8df0349165355b5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-19 18:14:25 +0300

    0.4.1

--- Controller/CustomerReturn/Index.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Dfe\GingerPayments\Controller\CustomerReturn;
-// 2017-03-07
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Index extends \Df\Payment\Action\CustomerReturn {}
