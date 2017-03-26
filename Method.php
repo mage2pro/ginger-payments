ce6c4db06346af52a60bdead0ead1f20ece12b30
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-26 14:11:56 +0300

    no message

--- Method.php
+++ Method.php
@@ -1,4 +1,4 @@
 <?php
-// 2017-02-25
 namespace Dfe\GingerPayments;
+// 2017-02-25
 final class Method extends \Df\GingerPaymentsBase\Method {}
--- W/Handler.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Dfe\GingerPayments\W;
-// 2017-03-09
-final class Handler extends \Df\GingerPaymentsBase\W\Handler {}
