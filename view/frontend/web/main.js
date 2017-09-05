9e59e2604a61fcabb8fb1662a1f1c57bf27e04de
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:30:55 +0300

    1.0.15

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,3 +1,3 @@
-// 2017-03-03
-// @see https://github.com/mage2pro/ginger-payments-base/blob/0.0.7/view/frontend/web/main.js?ts=4#L8
-define(['Df_GingerPaymentsBase/main'], function(parent) {return parent.extend({});});
+// 2017-03-03 
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
+define(['Df_GingerPaymentsBase/main'], function(parent) {return parent.extend();});
